76a381e73cd5ca860da12b173864de1dc490de4b
zhizhong.zhou <zhizhong.zhou@datayes.com>
2016-05-04 18:14:57 +0800

    cell

---
+++ Ping/General/Views/PTHistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  PTHistoryCell.swift
+//  Ping
+//
+//  Created by datayes on 16/5/4.
+//  Copyright © 2016年 Kejukeji. All rights reserved.
+//
+
+import UIKit
+
+class PTHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
